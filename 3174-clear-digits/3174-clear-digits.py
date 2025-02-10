d7b65603960951fcf7d58c54fd72dd0388918b29
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-02-10 06:46:19 +0200

    Update 3174-clear-digits.py

--- 3174-clear-digits/3174-clear-digits.py
+++ 3174-clear-digits/3174-clear-digits.py
@@ -4,17 +4,27 @@ class Solution:
         # def isdigit(c):
         #     return ord("0") <= ord(c) <= ord("9")
 
-        result = []
-        delete_counter = 0
+        # result = []
+        # delete_counter = 0
 
-        for i in reversed(range(len(s))):
+        # for i in reversed(range(len(s))):
+        #     if s[i].isdigit():
+        #         delete_counter +=1
+        #     elif delete_counter:
+        #         delete_counter -=1
+        #     else:
+        #         result.append(s[i])
+        # return "".join(result[::-1])
+
+        # second solution using delete stack:
+        result = []
+        for i in range(len(s)):
             if s[i].isdigit():
-                delete_counter +=1
-            elif delete_counter:
-                delete_counter -=1
+                result.pop()
             else:
                 result.append(s[i])
-        return "".join(result[::-1])
+
+        return "".join(result)
 
          
         
