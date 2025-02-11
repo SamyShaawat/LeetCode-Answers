8255cc55f081ce77ba8572692eed61c3dc1a5888
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-02-11 11:31:46 +0200

    Update 0027-remove-element.py

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,5 +1,15 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        while val in nums:
-            nums.remove(val)
-        return len(nums)
+        # using two pointers:
+        k = 0 # first pointer 
+        for i in range(len(nums)): # i is the secongd pointer
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k+=1
+        return k
+# another solution  
+# class Solution:
+#     def removeElement(self, nums: List[int], val: int) -> int:
+#         while val in nums:
+#             nums.remove(val)
+#         return len(nums)
