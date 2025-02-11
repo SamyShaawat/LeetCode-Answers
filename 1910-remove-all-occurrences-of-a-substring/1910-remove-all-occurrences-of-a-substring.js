19b92f01fbed1f31518526e592c0abb3bf1d8bd6
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-02-11 11:39:35 +0200

    Time: 1 ms (67.67%), Space: 51.2 MB (20.3%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string} part
+ * @return {string}
+ */
+var removeOccurrences = function(s, part) {
+    let resultStack = [];
+    let targetLength = part.length;
+    let targetEndChar = part[part.length - 1];
+
+    for (let currentChar of s) {
+        resultStack.push(currentChar);
+
+        if (currentChar === targetEndChar && resultStack.length >= targetLength) {
+            if (resultStack.slice(-targetLength).join('') === part) {
+                resultStack.length -= targetLength;
+            }
+        }
+    }
+    return resultStack.join('');
+};
