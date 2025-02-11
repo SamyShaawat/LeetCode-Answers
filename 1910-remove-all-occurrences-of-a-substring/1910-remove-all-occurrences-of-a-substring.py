3969b6687c3ecc41577b48eb8f090f658ba6f753
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-02-11 10:45:07 +0200

    Update 1910-remove-all-occurrences-of-a-substring.py

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.py
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.py
@@ -3,3 +3,12 @@ class Solution:
         while part in s:
             s = s.replace(part, "", 1)  # Removes the first occurrence of `part`
         return s
+# another solution 
+# class Solution:
+#     def removeOccurrences(self, s: str, part: str) -> str:
+#         while part in s:
+#             for i in range(len(s) - len(part) +1): # explaing why exactly choose this range 
+#                 window = s[i: i + len(part)] # here also why this range 
+#                 if window == part:
+#                     s = s.replace(window,"", 1) # what is replace do exactly 
+#         return s  
