2c5f1d6717183888a6ec7e500edd367add236510
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-02-17 11:46:42 +0200

    Time: 23 ms (85.23%), Space: 19.3 MB (37.53%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
@@ -1,19 +1,26 @@
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
-        hashmap = Counter(tiles)  # Count the occurrences of each tile
+        # hashmap = Counter(tiles)  # Count the occurrences of each tile
         
-        def backTrack():
-            res = 0  # Store the number of unique sequences formed
+        # def backTrack():
+        #     res = 0  # Store the number of unique sequences formed
             
-            for key in hashmap:  # Iterate through all unique tiles
-                if hashmap[key] > 0:  # Check if the tile is available for use
-                    hashmap[key] -= 1  # Use the tile (decrease its count)
-                    res += 1  # Count this new sequence
-                    res += backTrack()  # Explore further sequences using recursion
-                    hashmap[key] += 1  # Restore the tile count (backtracking)
+        #     for key in hashmap:  # Iterate through all unique tiles
+        #         if hashmap[key] > 0:  # Check if the tile is available for use
+        #             hashmap[key] -= 1  # Use the tile (decrease its count)
+        #             res += 1  # Count this new sequence
+        #             res += backTrack()  # Explore further sequences using recursion
+        #             hashmap[key] += 1  # Restore the tile count (backtracking)
             
-            return res  # Return the total count of sequences
+        #     return res  # Return the total count of sequences
         
-        return backTrack()  # Start the recursive function
+        # return backTrack()  # Start the recursive function
+        # ---------------------------------------------------------
+        #  another solution 
+        unique_set = set()
+        for i in range(1, len(tiles) + 1):
+            for p in permutations(tiles, i):
+                unique_set.add(p)
+        return len(unique_set)
             
         
