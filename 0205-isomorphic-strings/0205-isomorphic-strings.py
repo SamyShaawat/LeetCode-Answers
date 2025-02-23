3f3492a832a28a721cd0751a07d132fb36ab60f1
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-02-23 12:02:04 +0200

    Time: 11 ms (18.25%), Space: 18.2 MB (18.31%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,10 +1,16 @@
-class Solution:
-    def isIsomorphic(self, s: str, t: str) -> bool:
-        S = Counter(s)
-        T = Counter(t)
-        for key in S:
-            if S[key] in T.values():
-                continue
-            else:
-                return False
-        return True        
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        map1 = []
+        map2 = []
+        for char in s:
+            map1.append(s.index(char))
+            # print(s.index(char))
+        # print(map1)
+        for char in t:
+            map2.append(t.index(char))
+            # print(t.index(char))
+        # print(map2)
+        if map1 == map2:
+            return True
+        return False
+                
