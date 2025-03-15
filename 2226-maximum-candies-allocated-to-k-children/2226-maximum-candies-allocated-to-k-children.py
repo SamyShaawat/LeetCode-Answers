316cf598673581dda360da472c872088d4b1c7f8
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-03-15 10:12:50 +0200

    problemName: 2226-maximum-candies-allocated-to-k-children, Date: 03-15-2025, Time: 419 ms (40.75%), Difficulty: Medium, Language .py

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
@@ -1,15 +1,17 @@
 class Solution:
     def maximumCandies(self, candies: List[int], k: int) -> int:
+        if sum(candies) < k:
+            return 0
         l, r = 1, max(candies)
-        res = 0
+        best = 0
         while l <= r:
-            mid = (l+r) // 2
-            childern_count = sum(pile//mid for pile in candies)
+            mid = (l + r) // 2
+            childern_count = sum(pile//mid for pile in candies) # total children that can rec eive 'mid' candies
             print(childern_count)
             if childern_count >= k:
-                res = mid 
-                l = mid + 1
+                best = mid 
+                l = mid + 1 # try for a larger size
             else:
-                r = mid - 1 
-        return res
+                r = mid - 1 # try a smaller size
+        return best
         
