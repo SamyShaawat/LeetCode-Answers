6ce1cf280e9b0dbbd2fc09fe713ce17b5338fa22
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-03-26 22:10:44 +0200

    Date: 03-26-2025, problemName: 0006-zigzag-conversion, Difficulty: Medium, Language .py

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,14 +1,18 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        rows = [''] * numRows
-        print(rows)
-        k = 0
+        rows = [''] * numRows # list of empty strings to store characters for each row
+        # print(rows)
+        k = 0 # Index to track the current row
+
+        # Direction of movement (1 for down, -1 for up)
+        # If numRows is 1, direction will be 0 (no zigzag needed)
         direction = (numRows == 1) - 1
-        print(direction)
+        # print(direction)
         for c in s:
-            rows[k] += c
+            rows[k] += c # Append the character to the current row
+            # If we reach the first or last row, change direction
             if k == 0 or k == numRows - 1:
-                direction *= -1
-                print(direction)
-            k += direction
+                direction *= -1 # Reverse direction
+                # print(direction)
+            k += direction # Move to the next row in the current direction
         return ''.join(rows)
