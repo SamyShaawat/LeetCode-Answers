a505ffecee822f6293abacd5194be9b45504c5d8
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-03-26 17:58:54 +0200

    Date: 03-26-2025, problemName: 2108-find-first-palindromic-string-in-the-array, Difficulty: Easy, Language .py

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        for s in words:
+            if s[::] == s[::-1]:
+                return s
+            else:
+                continue
+        return ""
+        
