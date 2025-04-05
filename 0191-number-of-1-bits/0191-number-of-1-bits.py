87027c98afccf66d349ab149aa7f6043f87bdb95
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-04-05 13:48:13 +0200

    Date: 04-05-2025, problemName: 0191-number-of-1-bits, Difficulty: Easy, Language .py

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,5 +1,9 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
+        # binary_str = bin(n)
+        # print(binary_str) # 0b1011
         binary_str = bin(n)[2:]
+        # print(binary_str) # 1011
+
         return binary_str.count('1')
         
