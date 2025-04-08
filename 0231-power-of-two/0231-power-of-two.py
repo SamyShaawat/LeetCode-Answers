51fb5d91876de0fb3303fa55751ff0631f08d985
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-04-08 03:55:27 +0200

    Date: 04-08-2025, problemName: 0231-power-of-two, Difficulty: Easy, Language .py

---
+++ 0231-power-of-two/0231-power-of-two.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n > 0:
+            num = bin(n)[2::]
+        else:
+            num = bin(n)[3::]
+        # print(int(num, 2))
+        hashmap = Counter(num)
+        for key, value in hashmap.items():
+            if n > 0:
+                if key == '1':
+                    if value == 1:
+                        return True
+
+        return False
