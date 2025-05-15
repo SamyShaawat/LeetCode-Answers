d4ff587b0088b842b7525215fba23d92252a62a0
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-05-15 23:45:13 +0300

    Time: 0 ms (100%), Space: 17.7 MB (91.36%) - LeetHub

--- 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.py
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.py
@@ -1,25 +1,16 @@
+from typing import List
+
 class Solution:
-    def getLongestSubsequence(
-        self, words: List[str], groups: List[int]
-    ) -> List[str]:
-        n = len(words)
-        dp = [1] * n
-        prev = [-1] * n
-        max_len, end_index = 1, 0
+    def getLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
+        if not words:
+            return []
 
-        for i in range(1, n):
-            best_len, best_prev = 1, -1
-            for j in range(i - 1, -1, -1):
-                if groups[i] != groups[j] and dp[j] + 1 > best_len:
-                    best_len, best_prev = dp[j] + 1, j
-            dp[i] = best_len
-            prev[i] = best_prev
-            if dp[i] > max_len:
-                max_len, end_index = dp[i], i
+        result = [words[0]]
+        last_group = groups[0]
 
-        res = []
-        i = end_index
-        while i != -1:
-            res.append(words[i])
-            i = prev[i]
-        return res[::-1]
+        for i in range(1, len(words)):
+            if groups[i] != last_group:
+                result.append(words[i])
+                last_group = groups[i]
+
+        return result
