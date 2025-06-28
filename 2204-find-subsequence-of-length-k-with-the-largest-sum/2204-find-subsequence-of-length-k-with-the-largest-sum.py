47920525f8576cff97fcec109d2c93263dac037a
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-06-28 18:07:59 +0300

    Time: 35 ms (5.29%), Space: 17.9 MB (94.31%) - LeetHub

---
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        for i in range(n-k):
+            nums.remove(min(nums))
+        return nums
+        
