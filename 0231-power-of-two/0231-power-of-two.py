dd54411afc2a945837a891cb0e8bcab5b2c93fec
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-08-15 20:22:02 +0300

    Time: 0 ms (100%), Space: 17.7 MB (55.55%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,13 +1,22 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n > 0:
-            num = bin(n)[2::]
-        else:
-            num = bin(n)[3::]
-        hashmap = Counter(num)
-        for key, value in hashmap.items():
-            if n > 0:
-                if key == '1':
-                    if value == 1:
-                        return True
+        # if n > 0:
+        #     num = bin(n)[2::]
+        # else:
+        #     num = bin(n)[3::]
+        # hashmap = Counter(num)
+        # for key, value in hashmap.items():
+        #     if n > 0:
+        #         if key == '1':
+        #             if value == 1:
+        #                 return True
+        # return False
+        i = 0 
+        while (2**(i+1) <= n):
+            i+=1
+        while (i >=0):
+            power = 2**i
+            if power == n:
+                return True
+            i-=1
         return False
